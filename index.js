2ea10f2293b4beb538f7ae8a1d4bceff7322054f
Human-100 <107300415+Human-100@users.noreply.github.com>
2024-11-08 20:22:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const box = document.querySelector(".visits");
 async function updateCounter(){
-    let response = await fetch("https://d2wnzuiomynqnelo26czsywgxe0pusnm.lambda-url.ap-south-1.on.aws/");
+    let response = await fetch("Your Lambda Function");
     let data = await response.json();
     box.innerHTML = data;
 
